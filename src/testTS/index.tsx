9e90d92ebdbab9c1477082b2abce8aa5206f8f12
Sosin Dima <sosindima5@gmail.com>
2024-05-02 11:12:43 +0700

    Пример ts

---
+++ src/testTS/index.tsx
@@ -0,0 +1,25 @@
+import {AProps} from "./interfaces";
+import {FC} from "react";
+
+type AB = {
+    name: string
+}
+export const A: FC<AProps> = (props) => {
+    const {users} = props;
+
+    const calcSomething = (time: string): AB => {
+        return {name: 'a'}
+    }
+
+    return <>{users.map(({name, age, gender,isEblan, children}) => (
+        <div key={name}>
+            <span>{name}</span>
+            <span>{age}</span>
+            <span>{gender}</span>
+            <span>{isEblan}</span>
+            {children?.map(child => ...)}
+            {children && children.map(child => ...)}
+        </div>
+        )
+    )}</>
+}
---
+++ src/testTS/interfaces.ts
@@ -0,0 +1,60 @@
+enum GENDER {
+    M = 'M',
+    J = 'J'
+}
+
+type Child = {
+    ...
+}
+
+
+type User = {
+    /** Имя человека */
+    name: string;
+    /** Возраст */
+    age: number;
+    gender: GENDER
+    isEblan: boolean;
+    onCancel: (isOpen: boolean) => GENDER
+    // children?: Child[] | undefined | null
+    children?: Child[] | null
+}
+
+if (user.gender === GENDER.M ) {
+    ...
+}
+
+
+export type AProps = {
+    users: User[]
+}
+
+const users = [
+    {name: '1', age: 5, gender: GENDER.M, isEblan: true},
+    {name: '1', age: 4, gender: GENDER.J, isEblan: true}
+]
+
+const onCalcel = (isOpen: boolean) => {
+    ...
+
+    return GENDER
+}
+
+
+// interface
+// type
+
+
+// string
+// boolean
+// number
+// null
+// undefined
+
+
+// object
+// any
+// unknown
+
+// https://www.typescriptlang.org/docs/handbook/2/generics.html
+// https://www.typescriptlang.org/docs/handbook/utility-types.html Partial Required Readonly Record(+) Pick(+) Omit(+) Exclude
